1de3021e02d65f3081a71962ce4c37300570d6aa
daibigmonster <546508148@qq.com>
2017-12-13 14:09:49 +0800

    php文件设置

---
+++ 1712131_test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    $var_name='苹果';
+    $n = 10;
+
+    var_dump($var_name);
+    var_dump($n);
+?>
+</body>
+</html>
