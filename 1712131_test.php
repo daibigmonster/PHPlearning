163802da7fa94d81b6c0ea01ef9bd0e919723d6d
daibigmonster <546508148@qq.com>
2017-12-13 15:22:57 +0800

    php文件的改动

--- 1712131_test.php
+++ 1712131_test.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php 
+    echo "Hello world!\n";
+    echo "Hello world!\n";
     $var_name='苹果';
     $n = 10;
 
     var_dump($var_name);
     var_dump($n);
 ?>
-</body>
-</html>
