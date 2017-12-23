5797bdd2e123054b66451ceada2bad25b774ff37
daibigmonster <546508148@qq.com>
2017-12-23 21:41:26 +0800

    建立起第一个自己的网站

--- 1712131_test.php
+++ 1712131_test.php
@@ -1,8 +1,22 @@
-<?php 
-$string = "就是就是";
-var_dump($string);
-echo "<br />";
-$string = 9494;
-var_dump($string);
-echo "<br />";
+<?php
+    class Car {
+        function __construction(){
+            print "构造函数被调用\n";
+        }
+        function __destruction(){
+            print "析构函数被调用\n";
+        }
+        public $speed = 0;
+        //增加speedUp方法，使speed加10
+        public function speedUP(){
+            $this->speed += 10;
+        }
+        
+    }
+    $car = new Car();
+    while($car->speed <= 100){
+        $car->speedUp();
+        echo $car->speed."\n";
+    }
+    unset($car);
 ?>
